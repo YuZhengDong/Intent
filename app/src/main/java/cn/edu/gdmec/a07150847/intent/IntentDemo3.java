6d96d1d348a78e384eb82f85277840faba1dfbb0
gdmec07150847 <1103249267@qq.com>
2016-10-30 14:31:11 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/gdmec/a07150847/intent/IntentDemo3.java
@@ -0,0 +1,16 @@
+package cn.edu.gdmec.a07150847.intent;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by asus on 2016/10/29.
+ */
+public class IntentDemo3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent_demo3);
+    }
+}
